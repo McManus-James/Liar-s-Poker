c40261f73b106ea147d2464c837cc69a7ad86701
Michelle Ip <michelleip9@dhcp-rhodes-183.eduroam.cornell.edu>
2016-11-11 15:47:29 -0500

    started to write new_deck

--- deck.ml
+++ deck.ml
@@ -13,7 +13,19 @@ module type Deck = struct
 
   let empty = []
 
-  let new_deck e =
+  (* helper function to instantiate a 52 card deck.
+   * [suit] is a suit, and [rank_list] is a list of
+   * all possible ranks
+   * [accum] is an accumulator holding the deck created so far
+   * function returns all the cards for a particular suit, from 2 through Ace
+   * (represented as rank 14) *)
+  let deck_helper suit rank_list accum = match rank_list with
+    | [] -> accum
+    | h::t -> (h, suit)::accum
+
+  let new_deck suit_list accum = match suit_list with
+    | [] -> accum
+    | h::t -> (deck_helper h [1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13; 14] accum)@accum
   (* [shuffle_deck d] takes in a Deck [d] and returns a shuffled copy of the
    * original deck *)
   val shuffle_deck: deck -> deck
