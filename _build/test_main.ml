36126c02ea80f5fd25787262cf7b4516899dd294
Edward Bao <Ebao@dhcp-uris-2759.redrover.cornell.edu>
2016-11-28 11:54:01 -0500

    fixed only one round

---
+++ _build/test_main.ml
@@ -0,0 +1,12 @@
+open OUnit2
+open Modules
+open Game
+
+let tests = []
+
+let suite = "Final Project test suite" >:::
+  tests
+
+(* The following line must be the one and only place
+ * in your entire source code that calls [OUnit2.run_test_tt_main]. *)
+let _ = run_test_tt_main suite
