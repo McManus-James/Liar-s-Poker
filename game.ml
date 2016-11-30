5a64a2f2a71f6b00a976095b5bf6feb807018653
James McManus <jjm439@cornell.edu>
2016-11-30 14:48:47 -0500

    improved parser

--- game.ml
+++ game.ml
@@ -3,9 +3,9 @@ module MyRound = Pokergame.GameRound
 let rec play info =
   let loser = MyRound.play_round info in
   let new_info = MyRound.update_state loser info in
-  if List.length new_info.players = 1 then
-  new_info.cur_player
-  else play new_info
+  match MyRound.winner new_info with
+  | Some p -> p
+  | None -> play new_info
 
 let main n =
   let state = MyRound.init_state n in
