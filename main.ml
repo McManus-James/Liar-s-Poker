788cfac3b9f9def27737fec4944b19996dc92b84
Michelle Ip <michelleip9@dhcp-mcfad-6797.eduroam.cornell.edu>
2016-12-04 12:43:35 -0500

    Added error handling for when the player types in an invalid value for number of players or for difficulty

--- main.ml
+++ main.ml
@@ -1,20 +1,40 @@
+let rec get_number_players i =
+  try (print_endline "Please enter the number of players.";
+  print_string ">";
+  let num_players = read_line () |> int_of_string in
+  if (num_players < 2) then
+    (print_endline "The number of players must be at least 2. Please try again.";
+    get_number_players 0)
+  else if num_players > 9 then
+    (print_endline "The number of players must be at most 9. Please try again.";
+    get_number_players 0)
+  else num_players)
+  with Failure "int_of_string" -> (let () = print_endline ("The number of players must be "^
+                                 "an integer between 2 and 9. Please try again.") in
+                                 get_number_players 0)
 
-(* IMPORTANT NOTE:
- * You should not need to modify this file, though perhaps for karma
- * reasons you might choose to do so.  The reason this file is factored
- * out from [game.ml] is for testing purposes:  if we're going to unit
- * test the [Game] module, it can't itself invoke its [main] function;
- * if it did, the game would try to launch and would interfere with OUnit.
- *)
+let rec get_difficulty i =
+  try (print_endline ("Please enter the difficulty 1-3.");
+  print_string ">";
+  let diff = read_line () |> int_of_string in
+  if (diff < 1) then
+    (print_endline "The difficulty must be at least 1. Please try again.";
+    get_difficulty 0)
+  else if diff > 3 then
+    (print_endline "The difficulty must be at most 3. Please try again.";
+    get_difficulty 0)
+  else diff)
+  with Failure "int_of_string" -> (let () = print_endline ("The difficulty must be "^
+                                 "an integer between 1 and 3. Please try again.") in
+                                 get_difficulty 0)
+
 exception Invalidplayers
 exception Invaliddifficulty
 
 let () =
-  print_endline ("Welcome to Liar's Poker! "
-                ^"Please enter the number of players.\n");
-  print_string  "> ";
-  let num_players = int_of_string (read_line ()) in
-  print_endline ("Please enter the difficulty 1-3.\n");
-  print_string  "> ";
-  let diff = int_of_string (read_line ()) in
+  print_endline ("Welcome to Liar's Poker!");
+               (*  ^"Please enter the number of players.\n");
+  print_string  "> "; *)
+  let num_players = get_number_players 0 in
+  let diff = get_difficulty 0 in
   Game.main num_players diff
